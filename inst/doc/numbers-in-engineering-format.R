64489db1269a0a6c357cb93beb29c1e0b9797fc3
Richard Layton <graphdoctor@gmail.com>
2019-02-09 17:43:13 +0000

    version 0.2.1

--- inst/doc/numbers-in-engineering-format.R
+++ inst/doc/numbers-in-engineering-format.R
@@ -6,13 +6,7 @@ knitr::opts_chunk$set(
   warning = FALSE,
   collapse = TRUE,
   comment = "#>",
-  error = TRUE,
-  purl = FALSE,
-  fig.width = 6,
-  fig.asp = 1 / 1.6,
-  out.width = "70%",
-  fig.align = "center",
-  fig.path = "../man/figures/engr-format-"
+  error = TRUE
 )
 knitr::knit_hooks$set(inline = function(x) {
   if (!is.numeric(x)) {
@@ -24,4 +18,61 @@ knitr::knit_hooks$set(inline = function(x) {
   }
 })
 options(tibble.print_min = 8L, tibble.print_max = 8L)
+
+## ------------------------------------------------------------------------
+library("dplyr")
+library("knitr")
+library("docxtools")
+
+## ------------------------------------------------------------------------
+density
+
+## ------------------------------------------------------------------------
+map_chr(density, class)
+map_chr(density, typeof)
+
+## ------------------------------------------------------------------------
+density_engr <- format_engr(density)
+density_engr
+
+## ------------------------------------------------------------------------
+map_chr(density_engr, class)
+
+## ------------------------------------------------------------------------
+kable(density_engr)
+
+## ------------------------------------------------------------------------
+density_engr <- density %>%
+  format_engr()
+kable(density_engr)
+
+## ------------------------------------------------------------------------
+density_engr <- format_engr(density, sigdig = 3)
+kable(density_engr)
+
+## ------------------------------------------------------------------------
+density_engr <- format_engr(density, sigdig = c(5, 4, 0, 5))
+kable(density_engr)
+
+## ------------------------------------------------------------------------
+x <- density %>%
+  select(T_K, p_Pa, R, density)
+
+## ------------------------------------------------------------------------
+kable(format_engr(x, sigdig = 4), caption = "4 digits")
+
+## ------------------------------------------------------------------------
+kable(format_engr(x, sigdig = 3), caption = "3 digits")
+
+## ------------------------------------------------------------------------
+kable(format_engr(x, sigdig = 2), caption = "2 digits")
+
+## ------------------------------------------------------------------------
+kable(format_engr(x, sigdig = 2, ambig_0_adj = TRUE), caption = "Removing ambiguity")
+
+## ------------------------------------------------------------------------
+kable(format_engr(x, sigdig = c(4, 2, 0, 3), ambig_0_adj = FALSE))
+
+## ------------------------------------------------------------------------
+kable(format_engr(x, sigdig = c(4, 2, 0, 3), ambig_0_adj = TRUE))
 
